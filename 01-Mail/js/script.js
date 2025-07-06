2028e6ed1d96459d1671f247bad5f94ad158e2c0
biancapcr <biancacristina.pocora@gmail.com>
2025-07-06 11:51:04 +0200

    [add] ciclo for ex-01-mail

--- 01-Mail/js/script.js
+++ 01-Mail/js/script.js
@@ -8,3 +8,13 @@ const invitedEmails =[
 ];
 // 2. Richiesta dell'e-mail all'utente
 const userEmail = prompt("Inserisci la tua email:");
+
+// 3. Ricerca dell'e-mail nella lista
+let found = false;
+for (let i = 0; i < invitedEmails.length; i++){
+  // Confronto l'email corrente con quella inserita dall'utente
+  if (invitedEmails[i] === userEmail){
+    found = true;
+    break;         
+  }
+}

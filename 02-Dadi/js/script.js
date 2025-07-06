38165ccdc9b0be02ff693ba87347f36fdab00ed4
biancapcr <biancacristina.pocora@gmail.com>
2025-07-06 11:56:23 +0200

    [add] console.log ex-02-dadi

--- 02-Dadi/js/script.js
+++ 02-Dadi/js/script.js
@@ -1,3 +1,7 @@
 // 1. Generazione del lancio
 const playerRoll = Math.floor(Math.random() * 6) + 1;
 const computerRoll = Math.floor(Math.random() * 6) + 1;
+
+// 2. Stampa risultati
+console.log("Tuo lancio: " + playerRoll);
+console.log("Lancio computer: " + computerRoll);
